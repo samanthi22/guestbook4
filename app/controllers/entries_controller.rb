172c5211d5635d0b50c13d5ba0f3b2a181a32b61
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2018-08-04 19:26:03 +0000

    Doesn't add blanks if @name blank to db

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,11 @@
 class EntriesController < ApplicationController
     def sign_in
         @name = params[:visitor_name]
-        @entry = Entry.create({:name => @name})
+        unless @name.blank?
+            @entry = Entry.create({:name => @name})
+        end
+        
+        @entries = Entry.all
     end
 
 end
